c50ade26b863b60293e9803890be3b1cee5d221d
Yahyarashid <yahyomar63@gmail.com>
2018-11-30 13:48:17 +0200

    jjj

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
  app.use(express.static(__dirname + '/react-client/public'));
 //var data=[{Name:'asraf' ,HomeWork:'y7ya'}]
 
+   if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.get('/items', function (req, res) {
   students.selectAll(function(err, data) {
     if(err) {
