2cfba99ee2922de75c12c26351851e9c5a7e80c1
ashraf jibreel <ashrafjebril9@gmail.com>
2018-12-01 20:12:40 +0200

    get images to home page

--- react-client/src/components/Home.js
+++ react-client/src/components/Home.js
@@ -2,33 +2,49 @@ import React from 'react'
 
 const Home=(props)=>{
  const content = props.items.map((item) =>
-    <div >
-      <h3>{item.Name} &nbsp;&nbsp;&nbsp;&nbsp;{item.HomeWork}</h3>
+    <div className="images">
+		<a href="#" data-toggle="modal" data-target="#eventClicked">	<img src={item.Name}></img></a>
+      <div className="content">{item.HomeWork}</div>
+      
     </div>
+
+
   );
     return(
         
-        <div className="container-fluid page-cont">
-		<div className="row dash-row">
+  <div>
 			
-			<div className="col-4 data-box">
+			<div className="container-fluid" >
 				<div>
-				{content}
+          
+        {content}
+        
 				</div>
+				
 			</div>
-			
-			<div className="col-4 data-box">
-				<div>
-				</div>
-			</div>
-			
-			<div className="col-4 data-box">
-				<div>
-				</div>
-			</div>
-			
-		</div>
-	</div>
+     <div className="modal" id="eventClicked">
+     <div className="modal-dialog">
+       <div className="modal-content">
+       
+        
+         <div className="modal-header">
+           <h4 className="modal-title">Event Name</h4>
+           <button type="button" className="close" data-dismiss="modal">&times;</button>
+         </div>
+         
+         
+         <div className="modal-body">
+       
+         </div>
+         
+        
+         <div className="modal-footer">
+           <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+         </div>
+         
+       </div>
+     </div></div>
+              </div>
     )
 }
 
