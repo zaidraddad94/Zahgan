3255254ec0ac6475d7f11e1fefb2a68b7371366d
zaidraddad94 <zaidrddad@gmail.com>
2018-12-02 22:41:41 +0200

    feat show the events in data base for this creater

--- react-client/src/components/create.js
+++ react-client/src/components/create.js
@@ -2,51 +2,132 @@ import React from 'react'
 import $ from 'jquery';
 
 class Create extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {host: '',
-                  event:'',
-                  description : '',
-                  photo : '',
-                  sets: '',
-                  date:'',
-                  location : '' ,
-                };
+    constructor(props) {
+      super(props);
+      this.state = {
+        host: '',
+        event: '',
+        description: '',
+        photo: '',
+        sets: '',
+        date: '',
+        location: '',
+        items: '',
+      };
 
-   
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
-  
-
-  handleSubmit(event) {
-    var obj = {creatorName: this.state.host,
-      eventName:this.state.event,
-      des : this.state.description,
-      url : this.state.photo,
-      availableSeats: this.state.sets,
-    date:this.state.date,
-    eventLocation : this.state.location ,
-    attending:[]
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.allseats = this.allseats.bind(this);
     }
-    
-     console.log(obj)
-    $.ajax({
-      type: "POST",
-      url: '/create',
-      data: {obj},
-      success: function (xxx){
-        console.log(xxx)
-        } 
-     });
 
-    alert(obj.eventName + 'saved !' );
-    event.preventDefault();
+
+    componentDidMount() {
+      var that = this
+      $.ajax({
+        url: '/create',
+        success: (data) => {
+          console.log(data)
+          that.setState({
+            items: data
+          })
+        },
+        error: (err) => {
+          console.log('err', err);
+        }
+      });
+    }
+
+    handleSubmit(event) {
+      var obj = {
+        creatorName: this.state.host,
+        eventName: this.state.event,
+        des: this.state.description,
+        url: this.state.photo,
+        availableSeats: this.state.sets,
+        date: this.state.date,
+        eventLocation: this.state.location,
+        attending: []
+      }
+
+      console.log(obj)
+      $.ajax({
+        type: "POST",
+        url: '/create',
+        data: {
+          obj
+        },
+        success: function (xxx) {
+          console.log(xxx)
+        }
+      });
+
+      alert(obj.eventName + 'saved !');
+      $.ajax({
+        url: '/create',
+        success: (data) => {
+          console.log(data)
+          this.setState({
+            items: data
+          })
+        },
+        error: (err) => {
+          console.log('err', err);
+        }
+      });
+      event.preventDefault();
+    }
+    allseats(props) {
+      var lastindex = props.state.items.length - 1
+      var z = props.state.items[lastindex]
+      var zz = 0
+      for (var key in z) {
+        if (key === 'availableSeats') {
+
+          zz = z[key]
+        }
+      }
+      return zz
+    }
+
+    x(props) {
+      var t = props.state.items.length - 1
+      var z = props.state.items[t]
+      var zz = 0
+      for (var key in z) {
+        if (key === 'attending') {
+          
+          zz = z[key].length
+        }
+      }
+      return zz
+    }
+
+viewlest(props) {
+  var x = ""
+  {
+    var c = function (i) {
+      console.log( i.availableSeats)
+       var xx= i.availableSeats - i.attending.length
+    
+      x = x +  `${i.eventName} : ${xx} / ${i.availableSeats}   \n `
+    }
+    for (var i = 0; i < props.state.items.length; i++) {
+      c(props.state.items[i])
+    }
   }
+return x
+}
 
   render() {
     return (
       <div id="zz">
+      <h6> remaning sets for each event  : {"\n"} <h6>{this.viewlest(this)}</h6></h6>
+      <div className='dashbord'>evnts : {this.state.items.length}</div>
+      
+      
+      
+      
+      
       <h2> add event </h2>
       <form onSubmit={this.handleSubmit}>
         host name : <input 
@@ -94,3 +175,8 @@ class Create extends React.Component {
   }
 }
 export default Create
+
+// <div className='dashbord'> <h6>totla number of sets for ur last event:</h6> {  this.allseats(this)  }
+//       <h6>taken sets  :</h6> {this.x(this)}
+//       <h6>remaning sets :</h6> {this.allseats(this)-this.x(this)}
+//        </div>
