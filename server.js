b940aafe6a5b83b75b23c326486584c7ca0d6d59
Ghazi Majali <33878056+gmajali@users.noreply.github.com>
2018-12-02 18:04:10 +0200

    (feat) Sign In Post request working in server

--- server.js
+++ server.js
@@ -175,5 +175,73 @@ app.post('/account/signup', (req, res, next) => {
       });
     });
   });
+});
 
+// Signin User
+app.post('/account/signin', (req, res, next) => {
+  const { body } = req;
+  const {
+    password
+  } = body;
+  let {
+    email
+  } = body;
+
+  if (!email) {
+    return res.send({
+      success: false,
+      message: 'Error: Email cannot be blank.'
+    });
+  }
+  if (!password) {
+    return res.send({
+      success: false,
+      message: 'Error: Password cannot be blank.'
+    });
+  }
+
+  email = email.toLowerCase();
+
+  User.find({
+    email: email
+  }, (err, users) => {
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error: server error.'
+      });
+    }
+    if (users.length != 1) {
+      return res.send({
+        success: false,
+        message: 'Error: invalid.'
+      });
+    }
+
+    const user = users[0];
+    if(!user.validPassword(password)) {
+      return res.send({
+        success: false,
+        message: 'Error: invalid.'
+      });
+    }
+
+    // Otherwise correct user
+    const userSession = new UserSession();
+    userSession.userId = user._id;
+    userSession.save((err, doc) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: server error.'
+        });
+      }
+
+      return res.send({
+        success: true,
+        message: 'Valid sign in',
+        token: doc._id
+      });
+    })
+  });
 });
