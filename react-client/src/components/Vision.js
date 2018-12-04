0e37ba2e17778d8fd30dcf5bad88295a9ac47773
BatoolAlissa <43234063+BatoolAlissa@users.noreply.github.com>
2018-12-04 20:19:44 +0200

    clean up Vision

--- react-client/src/components/Vision.js
+++ react-client/src/components/Vision.js
@@ -2,82 +2,83 @@ import React from 'react'
 import zahgan from './zahgan.jpg';
 import Mission from './pages/Mission'
 
-const Vision=()=>{
+const Vision = () => {
 
-    return(
-   
-     
-<div class="container">
-    <h1>Service box</h1>
-    <div class="row">
-        <div class="col-md-3 col-sm-6 ">
-            <div class="service-box">
-                <div class="service-icon yellow">
-                    <div class="front-content">
-                        <i class="glyphicon glyphicon-grain"></i>
-                        <h3>mission</h3>
+    return (
+
+
+        <div class="container">
+            <h1>Service box</h1>
+            <div class="row">
+                <div class="col-md-3 col-sm-6 ">
+                    <div class="service-box">
+                        <div class="service-icon red">
+                            <div class="front-content">
+                                <i class="glyphicon glyphicon-grain"></i>
+                                <h3>mission</h3>
+                            </div>
+                        </div>
+                        <div class="service-content">
+                            <h3>design</h3>
+                            <p>Our mission is to provide the best event services
+                        <br />
+                                <u> <a href="/Mission">read more</a></u>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="service-content">
-                    <h3>design</h3>
-                    <p>Our mission is to provide the best event services 
-                        <br/>
-                       <u> <a href="/Mission">read more</a></u> 
-                    </p>
+                <div class="col-md-3 col-sm-6 ">
+                    <div class="service-box">
+                        <div class="service-icon red">
+                            <div class="front-content">
+                                <i class="glyphicon glyphicon-globe	"></i>
+                                <h3>php</h3>
+                            </div>
+                        </div>
+                        <div class="service-content">
+                            <h3>php developer</h3>
+                            <p>Our mission is to provide the best event services
+                        <br />
+                                <u>read more</u>
+                            </p>                </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="service-box ">
+                        <div class="service-icon red">
+                            <div class="front-content">
+                                <i class="fa fa-trophy"></i>
+                                <h3>Ui Developer</h3>
+                            </div>
+                        </div>
+                        <div class="service-content">
+                            <h3>Developer</h3>
+                            <p>Our mission is to provide the best event services
+                        <br />
+                                <u>read more</u>
+                            </p>                </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="service-box">
+                        <div class="service-icon red">
+                            <div class="front-content">
+                                <i class="glyphicon glyphicon-earphone"></i>
+                                <h3>Help</h3>
+                            </div>
+                        </div>
+                        <div class="service-content">
+                            <h3>Help</h3>
+                            <p>
+                            Our team will be happy to help you contact us at
+                            <br/>
+                                <u> 06-5515383 </u>
+                            </p>                </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3 col-sm-6 ">
-            <div class="service-box">
-                <div class="service-icon orange">
-                    <div class="front-content">
-                        <i class="glyphicon glyphicon-wrench"></i>
-                        <h3>php</h3>
-                    </div>
-                </div>
-                <div class="service-content">
-                    <h3>php developer</h3>
-                    <p>Our mission is to provide the best event services 
-                        <br/>
-                       <u>read more</u> 
-                    </p>                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-            <div class="service-box ">
-                <div class="service-icon red">
-                    <div class="front-content">
-                        <i class="fa fa-trophy"></i>
-                        <h3>Ui Developer</h3>
-                    </div>
-                </div>
-                <div class="service-content">
-                    <h3>Developer</h3>
-                    <p>Our mission is to provide the best event services 
-                        <br/>
-                       <u>read more</u> 
-                    </p>                </div>
-            </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
-            <div class="service-box">
-                <div class="service-icon grey">
-                    <div class="front-content">
-                        <i class="fa fa-paper-plane-o"></i>
-                        <h3>java script</h3>
-                    </div>
-                </div>
-                <div class="service-content">
-                    <h3>java script</h3>
-                    <p>Our mission is to provide the best event services 
-                        <br/>
-                       <u>read more</u> 
-                    </p>                </div>
-            </div>
-        </div>
-    </div>
-</div>
-   
+
 
     )
 }
