6a036189d1724548334e4ab3955e65ccbaf8fa40
Yahyarashid <yahyomar63@gmail.com>
2018-12-05 17:56:36 +0200

    fix map

--- react-client/src/components/Nav.js
+++ react-client/src/components/Nav.js
@@ -3,45 +3,45 @@ import zahgan from './zahgan.jpg';
 import $ from 'jquery';
 
 const ToEvents=()=>{
-    $("#clickEvent").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#events").offset().top
-        }, 2000);
-    });
+   $("#clickEvent").click(function() {
+       $('html, body').animate({
+           scrollTop: $("#events").offset().top
+       }, 2000);
+   });
 }
 
 const ToAbout=()=>{
-    $("#clickAbout").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#about").offset().top
-        }, 2000);
-    });
+   $("#clickAbout").click(function() {
+       $('html, body').animate({
+           scrollTop: $("#about").offset().top
+       }, 2000);
+   });
 }
 
 
 const Nav=()=>{
 
-    return(
-      <div>
-      <nav className="navbar navbar-expand-sm ">
-  <ul className="navbar-nav">
-            <li className="active"><a href="/homeClass">Home</a></li>
-            
-            <li><a href="#" onClick={ToAbout} id="clickAbout">About</a></li>
-            <li><a href="#">Contact us</a></li>
-            <img src={zahgan}></img>
-            <li><a href="/SignInCreator">Create</a></li>
-            <li><a href="/signin">Sign in</a></li>
-            <li><a href="javascript:void(0);" onClick={ToEvents} id="clickEvent">Events</a></li>
-           
-           < li><a href="/signup">Sign up</a></li>
-            
-          </ul>
-       
-      </nav>
+   return(
+     <div>
+     <nav className="navbar navbar-expand-sm ">
+ <ul className="navbar-nav">
+           <li className="active"><a href="/homeClass">Home</a></li>
 
-   </div>
+           <li><a href="#" onClick={ToAbout} id="clickAbout">About</a></li>
+           <li><a href="#">Contact us</a></li>
+           <img src={zahgan}></img>
+           <li><a href="/SignInCreator">Create</a></li>
+           <li><a href="/signin">Sign in</a></li>
+           <li><a href="javascript:void(0);" onClick={ToEvents} id="clickEvent">Events</a></li>
 
-    )
+          < li><a href="/signup">Sign up</a></li>
+
+         </ul>
+
+     </nav>
+
+  </div>
+
+   )
 }
 export default Nav
--- react-client/src/components/map.js
+++ react-client/src/components/map.js
@@ -1,50 +1,92 @@
+// import React, { Component } from 'react';
+// import GoogleMapReact from 'google-map-react';
+// import Marker from 'google-map-marker'
+ 
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+// class SimpleMap extends Component {
+//   static defaultProps = {
+//     center: {
+//       lat: 33.7496844,
+//       lng:-84.7516932,
+     
+//     },
+//     Marker : "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png",
+//     zoom: 20
+//   };
+
+
+// handleClickedMap = (e) => {
+//     console.log(e)
+//    let latitude = e.lat
+//    let longtitude  = e.lng
+//    console.log(latitude, longtitude)
+// }
+
+//   render() {
+//     return (
+//       // Important! Always set the container height explicitly
+//       <div style={{ height: '100vh', width: '100%' }}>
+//         <GoogleMapReact onClick={this.handleClickedMap } 
+//           bootstrapURLKeys={{ key:"AIzaSyD2IjGONmJ7Si4cNEZtNPNgPy5pVEt-_14"}}
+//           defaultCenter={this.props.center}
+//           defaultIcon={this.props.Marker}
+//           defaultZoom={this.props.zoom}
+//         >
+//           <AnyReactComponent
+//             lat={ 33.7496844}
+//             lng={-84.7516932}
+//             text='Hello world'
+            
+//           />
+//         </GoogleMapReact>
+//       </div>
+//     );
+//   }
+// }
+ 
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
  
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 41.882788232958994,
-      lng:-87.78338810258128,
-      icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    },
-    zoom: 11
-  };
+export class SimpleMap extends Component {
+//    handleClickedMap = (e) => {
+//     console.log(e)
+//    let latitude = e.initialCenter.lat
+//    let longtitude  = e.initialCenter.lng
+//    console.log('yyy',latitude,longtitude)
+// //    let longtitude  = e.lng
+// //    console.log(latitude, longtitude)
+// }
 
-//   Google.maps.event.addListener(map, "click", function (e) {
-
-//     //lat and lng is available in e object
-//     var latLng = e.latLng;
-
-// });
-
-handleClickedMap = (e) => {
-    console.log(e)
-   let latitude = e.lat
-   let longtitude  = e.lng
-   console.log(latitude, longtitude)
-}
+mapClicked(mapProps, map, clickEvent) {
+    console.log('bala',mapProps)
+    console.log('map',map);
+    console.log('mapprops',mapProps)
+  }
 
   render() {
+      //console.log('hhh',this.props.google)
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '70%' }}>
-        <GoogleMapReact onClick={this.handleClickedMap } 
-          bootstrapURLKeys={{ key:"AIzaSyD2IjGONmJ7Si4cNEZtNPNgPy5pVEt-_14"}}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={9.570833064972245}
-            lng={44.053840896358}
-            icon={'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'}
-          />
-        </GoogleMapReact>
-      </div>
+      <Map    onClick={this.mapClicked}  google={this.props.google}
+      initialCenter={{
+        lat: 0,
+        lng: 0
+      }}
+      zoom={11}
+     >
+ 
+        <Marker  
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+           
+        </InfoWindow>
+      </Map>
     );
   }
 }
- 
-export default SimpleMap;
+
+export default GoogleApiWrapper({
+  apiKey:"AIzaSyD2IjGONmJ7Si4cNEZtNPNgPy5pVEt-_14"
+})(SimpleMap)
+
