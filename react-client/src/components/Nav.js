d03fb77e5b5757a82348b1d99a23ad77b135645e
Ghazi Majali <33878056+gmajali@users.noreply.github.com>
2018-12-05 18:23:47 +0200

    (refactor) Refactor server.js and Nav.js

--- react-client/src/components/Nav.js
+++ react-client/src/components/Nav.js
@@ -24,17 +24,15 @@ const Nav=()=>{
     return(
       <div>
       <nav className="navbar navbar-expand-sm ">
-  <ul className="navbar-nav">
+        <ul className="navbar-nav">
             <li className="active"><a href="/homeClass">Home</a></li>
-            
             <li><a href="#" onClick={ToAbout} id="clickAbout">About</a></li>
             <li><a href="#">Contact us</a></li>
             <img src={zahgan}></img>
             <li><a href="/SignInCreator">Create</a></li>
             <li><a href="/signin">Sign in</a></li>
             <li><a href="javascript:void(0);" onClick={ToEvents} id="clickEvent">Events</a></li>
-           
-           < li><a href="/signup">Sign up</a></li>
+            <li><a href="/signup">Sign up</a></li>
             
           </ul>
        
