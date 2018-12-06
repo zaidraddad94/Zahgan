a786375709ab7eeb85ed74ed21a23343a909050f
ashraf jibreel <ashrafjebril9@gmail.com>
2018-12-06 10:59:08 +0200

    (fix) dashboard creator

--- react-client/src/components/Creator/SignInCreator.js
+++ react-client/src/components/Creator/SignInCreator.js
@@ -53,7 +53,7 @@ class SignInCreator extends React.Component {
     return (
       <div className="wrapper">
     <form className="form-signin" onSubmit={this.handleSubmit}>       
-      <h2 className="form-signin-heading">Please login</h2>
+      <h2 className="form-signin-heading">Event Manager login</h2>
       <input type="text" className="form-control" name="email" placeholder="Email Address" required="" autofocus="" value={this.state.email} onChange={e=>this.setState({email:e.target.value})} />
       <input type="password" className="form-control" name="password" placeholder="Password" required="" value={this.state.password} onChange={e=>this.setState({password:e.target.value})}/>      
       <button className="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
