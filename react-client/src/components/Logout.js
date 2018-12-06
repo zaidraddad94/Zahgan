429740444e55671ee5eefd663a72e4b0d26388f3
Ghazi Majali <33878056+gmajali@users.noreply.github.com>
2018-12-06 12:38:04 +0200

    (refactor) Refactor Signin, Logout, HomeClass, add JSON Webtoken

--- react-client/src/components/Logout.js
+++ react-client/src/components/Logout.js
@@ -1,50 +1,45 @@
-// import React from 'react'
-// import $ from 'jquery';
+import React from 'react'
+import $ from 'jquery';
 
-// class Logout extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         email: '',
-//         password: ''
-//       };
+class Logout extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      };
 
-//       this.handleSubmit = this.handleSubmit.bind(this);
-//     }
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-//     handleSubmit(event) {
-//       var obj = {
-//         email: this.state.email,
-//         password: this.state.password
-//       }
+    handleSubmit(event) {
+      $.ajax({
+        type: "POST",
+        url: '/account/signout',
+        headers: {
+            token: localStorage.getItem('token')
+        },
+        success: function (res) {
+          console.log(res)
+          alert(res.message)
+        }
+      });
 
-//       console.log(obj)
-//       $.ajax({
-//         type: "POST",
-//         url: '/account/signin',
-//         data: {
-//           email: obj.email,
-//           password: obj.password
-//         },
-//         success: function (res) {
-//           console.log(res)
-//           alert(res.message)
-//         }
-//       });
-
-//       event.preventDefault();
-//     }
+      event.preventDefault();
+    }
     
 
-//   render() {
-//     return (
-//       <div id="zz" className="container-fluid" >
-//       <form onSubmit={this.handleSubmit}>
-//         <button>Logout</button>
-//       </form>
-//       </div>
-//     );
-//   }
-// }
-// export default Logout
+  render() {
+    
+        // return  <Redirect to={{
+        //     pathname: '/homeClass',
+        //   }} />
+    return (
+      <div id="zz" className="container-fluid" >
+      <form onSubmit={this.handleSubmit}>
+        <button>Logout</button>
+      </form>
+      </div>
+    );
+  }
+}
+export default Logout
 
