5430e876a93a56158e98491c7b40d4f1def1187e
BatoolAlissa <43234063+BatoolAlissa@users.noreply.github.com>
2018-12-07 11:37:42 +0200

    cleanup vision

--- react-client/src/components/about/pages/location.js
+++ react-client/src/components/about/pages/location.js
@@ -5,16 +5,12 @@ const location = () => {
     return (
 
 
-        <div className="location container">
+        <div className="location">
             <main className=" m-0 p-0">
-                <div className="container-fluid m-0 p-0">
-                    <div className="z-depth-1-half map-container-4 map-container-google-4 height: 500px">
-                    <h1>Our Location </h1>
+            <div className='loc-title '> <h1 className='display-3'>Our Location </h1></div>
+                    <div className="z-depth-1-half map-container-4 map-container-google-4 map">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3384.051567103805!2d35.83549401509962!3d31.986617531171028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ca18e39fe576b%3A0xf308701edd298519!2sRBK+Hacker+Haus!5e0!3m2!1sen!2sjo!4v1543954476718"
-                            frameborder="0"
                             allowfullscreen></iframe>
-                    </div>
-
                 </div>
             </main>
         </div>
